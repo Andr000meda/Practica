5fbf9392c885a92b601b94b2fecb04c67f302180
Kitaro <119483957+Andr000meda@users.noreply.github.com>
2023-11-21 22:30:23 -0600

    Agregé todo el proyecto por primera vez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+import { Error404Component } from './components/error-404/error-404.component';
+
+// modulos
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListProductsComponent,
+    AddEditProductComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-edit-product',
+  templateUrl: './add-edit-product.component.html',
+  styleUrls: ['./add-edit-product.component.css']
+})
+
+
+export class AddEditProductComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [null, Validators.required],
+      stock: [null, Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+    }
+  addProduct() {
+    //  const product: Product = {
+    // }
+  }
+}
